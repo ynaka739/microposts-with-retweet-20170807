040d1cc5cb632cb19fa8051824eb8d25da78e88b
Y.N <y.naka.111.739@gmail.com>
2017-07-30 07:05:48 +0000

    201707301605

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, dependent: :destroy
-  has_many :followings, through: :relationships, source: :follow, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverses_of_relationship, source: :user, dependent: :destroy
+  has_many :followers, through: :reverses_of_relationship, source: :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_microposts, through: :favorites, source: :micropost
   
